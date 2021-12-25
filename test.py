3d5bb8755b07b109ac1f6b561c05f7741f0502f1
xihajun <mw18386@my.bristol.ac.uk>
2021-12-26 06:11:37 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+# This is just a test
+
+import pychromecast
+
+services, browser = pychromecast.discovery.discover_chromecasts()
+
+# Shut down discovery
+pychromecast.discovery.stop_discovery(browser)
